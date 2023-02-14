c43c9556ceacc6697249d7ca09059647c7a7a856
Li Nguyen <li.nguyenduong@gmail.com>
2023-02-14 15:27:50 +0700

    Update project structure

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,15 @@
-FROM gradle:7.3.3-jdk11 AS cache
-RUN mkdir -p /home/gradle/cache_home
-ENV GRADLE_USER_HOME /home/gradle/cache_home
-COPY build.gradle gradle.properties settings.gradle /home/gradle/app/
-COPY gradle /home/gradle/app/gradle
-
-WORKDIR /home/gradle/app
-RUN gradle build -Pspeed=true --no-daemon -i --stacktrace
-
 FROM gradle:7.3.3-jdk11 AS build
-COPY --from=cache /home/gradle/cache_home /home/gradle/.gradle
-COPY . /usr/src/app/
-WORKDIR /usr/src/app
-#Uncomment if need to check before build jar
-#RUN gradle clean check
-RUN gradle -Pdocker clean bootJar -i --stacktrace --build-cache
+COPY --chown=gradle:gradle . /home/gradle/src/
+
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon
 
 FROM openjdk:11.0.13-jre-slim
+
 EXPOSE 8080
-#TODO Run Docker as a non-root user
-USER root
-WORKDIR /usr/src/java-app
-COPY --from=build /usr/src/app/build/libs/*.jar ./app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"]
+
+RUN mkdir /app
+
+COPY --from=build /home/gradle/src/build/libs/rmt-auction-0.0.1-SNAPSHOT.jar /app/java.jar
+
+ENTRYPOINT ["java","-jar","/app/java.jar"]
--- src/main/java/com/nals/auction/config/WebSecurityConfiguration.java
+++ src/main/java/com/nals/auction/config/WebSecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.nals.auction.security.jwt.TokenProvider;
 import com.nals.utils.security.custom.CustomDefaultWebSecurityExpressionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,21 +16,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.filter.CorsFilter;
-import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 import tech.jhipster.config.JHipsterProperties;
 
 import static com.nals.utils.constants.Constants.ROLE_ADMIN;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
-@Import(SecurityProblemSupport.class)
-public class WebSecurityConfiguration
-    extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final JHipsterProperties jHipsterProperties;
     private final CorsFilter corsFilter;
     private final TokenProvider tokenProvider;
-    private final SecurityProblemSupport problemSupport;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,46 +36,45 @@ public class WebSecurityConfiguration
     @Override
     public void configure(final WebSecurity web) {
         web.ignoring()
-           .antMatchers(HttpMethod.OPTIONS, "/**")
-           .antMatchers("/app/**/*.{js,html}")
-           .antMatchers("/i18n/**")
-           .antMatchers("/content/**")
-           .antMatchers("/swagger-ui/**")
-           .antMatchers("/test/**");
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/app/**/*.{js,html}")
+                .antMatchers("/i18n/**")
+                .antMatchers("/content/**")
+                .antMatchers("/swagger-ui/**")
+                .antMatchers("/test/**");
     }
 
     @Override
-    public void configure(final HttpSecurity http)
-        throws Exception {
+    public void configure(final HttpSecurity http) throws Exception {
 
         // @formatter:off
-        http
-            .csrf()
-            .disable()
-            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling()
-            .authenticationEntryPoint(problemSupport)
-            .accessDeniedHandler(problemSupport)
-            .and()
-            .headers()
-            .xssProtection()
-            .and()
-            .contentSecurityPolicy(jHipsterProperties.getSecurity().getContentSecurityPolicy())
-            .and()
-            .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
-            .and()
-            .frameOptions()
-            .deny()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-            .expressionHandler(new CustomDefaultWebSecurityExpressionHandler())
-            .antMatchers("/api/**/auctions/su/**").hasAnyRole(ROLE_ADMIN)
-            .antMatchers("/api/**").authenticated()
-            .and()
-            .apply(securityConfigurerAdapter());
+        http.csrf()
+                .disable()
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .and()
+                .headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy(jHipsterProperties.getSecurity().getContentSecurityPolicy())
+                .and()
+                .referrerPolicy(
+                        ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+                .and()
+                .frameOptions()
+                .deny()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .expressionHandler(new CustomDefaultWebSecurityExpressionHandler())
+                .antMatchers("/api/**/auctions/su/**")
+                .hasAnyRole(ROLE_ADMIN)
+                .antMatchers("/api/**")
+                .authenticated()
+                .and()
+                .apply(securityConfigurerAdapter());
         // @formatter:on
     }
 
